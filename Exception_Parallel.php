fe04c69c351edb41510f165e7674306b388c6e60
parida007 <sibun.parida@gmail.com>
2019-09-27 15:00:00 +0530

    Delete Exception_Parallel.php

--- Exception_Parallel.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<h3><font color='red'>Currently Execution is going for different user.</font></h3>";
-echo "<br/><br/>";
-echo "<a href='index.html'><font size='5'> Go to Home Page</font></a>";
-
