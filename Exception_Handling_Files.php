8e7e535c3439c8a5cff8765e9001d26919783de8
parida007 <sibun.parida@gmail.com>
2019-09-27 14:59:50 +0530

    Delete Exception_Handling_Files.php

--- Exception_Handling_Files.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<h3><font color='red'>Give correct sequences for LncRNA and mRNA.</font></h3>";
-echo "<br/><br/>";
-echo "<a href='Cleaning_Ops.php'><font size='5'> Go to Home Page</font></a>";
-
